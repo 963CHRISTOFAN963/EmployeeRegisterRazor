aea27e5cc77f2342dfff3db292dac22dcfa50472
cmarcelino <cmarcelino@N-111>
2026-01-04 19:18:35 -0300

    Adicionar arquivos de projeto.

---
+++ Registrar_Funcionario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Registrar_Funcionario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
